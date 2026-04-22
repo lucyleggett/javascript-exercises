408b67c21f0f7f869e7e340203a4aa1c22a241af
lucyleggett <lucy.leggett@hotmail.com>
2026-04-22 13:55:36 +0800

    Solve the task

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+const repeatString = function(str, num) {
+    let repeated = "";
+    while (num > 0) {
+        num--;
+        repeated += str;
+    }
+    return repeated;
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
