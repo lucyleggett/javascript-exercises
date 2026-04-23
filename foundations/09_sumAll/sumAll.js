58efed8a47eb84faf25121128ebab3bd23439cb7
lucyleggett <lucy.leggett@hotmail.com>
2026-04-23 09:58:30 +0800

    Solve task

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 
+        || num2 < 0
+        || num1 % 1 !== 0
+        || num2 % 1 !== 0
+        || typeof(num1) !== "number"
+        || typeof(num2) !== "number"
+    ){
+        return "ERROR";
+    }
+    
+    let inputArray = [num1, num2];
+    let ascendingArray = inputArray.sort((a, b) => a - b);
+    let lowNum = ascendingArray[0];
+    let highNum = ascendingArray[1];
+    let numArray = [];
 
+    for (i = lowNum; i <= highNum; i++) {
+        numArray.push(i);
+    }
+    let sum = numArray.reduce((sum, current) => sum + current, 0);
+    return sum;
 };
 
 // Do not edit below this line
