43317547fd2252f6470db5e62a31f3c669b52dbb
lucyleggett <lucy.leggett@hotmail.com>
2026-04-23 10:48:18 +0800

    Solve task

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FahrenheitTemp) {
+  result = (FahrenheitTemp - 32) * (5 / 9);
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CelciusTemp) {
+  result = (CelciusTemp * (9 / 5) + 32);
+  return Math.round(result * 10) / 10;
 };
 
 // Do not edit below this line
