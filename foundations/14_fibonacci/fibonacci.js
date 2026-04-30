2520d2d027453139c330119728c30aa6cb70d686
lucyleggett <lucy.leggett@hotmail.com>
2026-04-30 18:26:35 +0800

    Solve task

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let defNum;
 
+    if (typeof num !== 'number') {
+        defNum = parseInt(num)
+    } else {
+        defNum = num
+    }
+
+    if (defNum < 0) return "OOPS";
+    if (defNum == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (i = 2; i <= num; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    
+    return firstPrev;
 };
 
 // Do not edit below this line
