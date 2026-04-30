67d451a271289f52c743f50b5acff694db4801fb
lucyleggett <lucy.leggett@hotmail.com>
2026-04-30 18:07:15 +0800

    Solve task

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const mappedInput = input
+    .toLowerCase()
+    .replaceAll(" ", "")
+    .replaceAll(/[^a-zA-Z0-9\s]/g, "")
+    .split("")
+    .join("");
 
+    const mappedBackwards = mappedInput.split("").reverse().join("");
+
+    return mappedInput === mappedBackwards;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
