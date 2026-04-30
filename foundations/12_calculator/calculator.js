776cb7074b87390a0c81e8a0a37ae83bbfef9509
lucyleggett <lucy.leggett@hotmail.com>
2026-04-30 17:05:10 +0800

    Solve tasks

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	const sum = n1 + n2
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	const product = n1 - n2;
+  return product;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const total = array.reduce((sum, value) => sum + value, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	const total = array.reduce((sum, value) => sum * value, 1);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	const result = n1 ** n2;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n1) {
+	let array = [];
+  for (i = n1; i >= 1; i--) {
+    array.push(i)
+  }
+  const factorial = array.reduce((acc, value) => acc * value, 1);
+  return factorial;
 };
 
 // Do not edit below this line
