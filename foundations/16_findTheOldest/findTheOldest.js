3c14ada42d49e31020bf57182f50bd42f758c1e9
lucyleggett <lucy.leggett@hotmail.com>
2026-05-03 13:32:28 +0800

    Solve task

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+        person.yearOfDeath ??= new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
-};
+const findTheOldest = function (people) {
+const ranked = people.toSorted(
+(person, nextPerson) => getAge(nextPerson) - getAge(person),
+);
+
+const oldest = ranked[0];
+return oldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
