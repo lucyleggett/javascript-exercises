b233b1cea320ec81532785e715adb3985cc36222
lucyleggett <lucy.leggett@hotmail.com>
2026-05-03 12:54:39 +0800

    Solve task

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const titles = array.map(item => item.title);
+    return titles;
 };
 
 // Do not edit below this line
